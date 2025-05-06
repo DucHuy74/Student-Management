4c96b6e94ad79e99691d4f7a25855347ad010877
DucHuy74 <duchuyhl74205@gmail.com>
2025-05-06 21:55:39 +0700

    create proj

---
+++ src/test/java/vn/proj/spring/studentmanage/StudentmanageApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.proj.spring.studentmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentmanageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
